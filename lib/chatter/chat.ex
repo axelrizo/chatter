e2183e0f5c905db6ecbe3d4600f03125ee1b4ee7
Axel Rizo <axelg.rizos@gmail.com>
2024-07-20 13:15:02 -0500

    feat: Add new chat room creation functionality and templates

--- lib/chatter/chat.ex
+++ lib/chatter/chat.ex
@@ -1,8 +1,23 @@
 defmodule Chatter.Chat do
-  alias Chatter.Chat
+  alias Chatter.Chat.Room
   alias Chatter.Repo
 
   def all_rooms do
-    Chat.Room |> Repo.all()
+    Room |> Repo.all()
+  end
+
+  def new_chat_room do
+    %Room{}
+    |> Room.changeset(%{})
+  end
+
+  def create_chat_room(params) do
+    %Room{}
+    |> Room.changeset(params)
+    |> Repo.insert()
+  end
+
+  def find_room(id) do
+    Room |> Repo.get!(id)
   end
 end
