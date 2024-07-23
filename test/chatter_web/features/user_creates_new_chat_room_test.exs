6abafffc0aa69ce7c5fc9ed639804166e5a37ff2
Axel Rizo <axelg.rizos@gmail.com>
2024-07-23 10:08:43 -0500

    feat: Add sign-in functionality to user_creates_new_chat_room_test

--- test/chatter_web/features/user_creates_new_chat_room_test.exs
+++ test/chatter_web/features/user_creates_new_chat_room_test.exs
@@ -2,14 +2,22 @@ defmodule ChatterWeb.Features.UserCreatesNewChatRoomTest do
   use ChatterWeb.FeatureCase, async: true
 
   test "user creates a new chat room successfully", %{session: session} do
+    user = build(:user) |> set_password("superpass") |> insert()
+
     session
-    |> visit(rooms_index())
+    |> visit("/")
+    |> sign_in(as: user)
     |> click(new_chat_link())
     |> create_chat_room(name: "elixir")
     |> assert_has(room_title("elixir"))
   end
 
-  defp rooms_index, do: Routes.chat_room_path(@endpoint, :index)
+  defp sign_in(session, as: user) do
+    session
+    |> fill_in(Query.text_field("Email"), with: user.email)
+    |> fill_in(Query.text_field("Password"), with: user.password)
+    |> click(Query.button("Sign in"))
+  end
 
   defp new_chat_link, do: Query.link("New chat room")
 
