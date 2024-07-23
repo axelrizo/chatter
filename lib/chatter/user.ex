c69b410b0517fa3681461362d5a8ce1588efe189
Axel Rizo <axelg.rizos@gmail.com>
2024-07-23 10:08:07 -0500

    feat: Add Chatter.User module for user management

---
+++ lib/chatter/user.ex
@@ -0,0 +1,21 @@
+defmodule Chatter.User do
+  use Ecto.Schema
+  import Ecto.Changeset
+
+  schema "users" do
+    field :email, :string
+    field :hashed_password, :string
+    field :session_secret, :string
+    field :password, :string, virtual: true
+
+    timestamps()
+  end
+
+  @doc false
+  def changeset(user, attrs) do
+    user
+    |> cast(attrs, [:email, :hashed_password, :session_secret])
+    |> validate_required([:email, :hashed_password, :session_secret])
+    |> unique_constraint(:email)
+  end
+end
