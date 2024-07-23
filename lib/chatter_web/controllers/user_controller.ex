c93f71ee8fbd1549d060aa3719e5bc8b1637a5fe
Axel Rizo <axelg.rizos@gmail.com>
2024-07-23 15:09:14 -0500

    refactor: user creation endpoint to handle invalid data

--- lib/chatter_web/controllers/user_controller.ex
+++ lib/chatter_web/controllers/user_controller.ex
@@ -1,22 +1,16 @@
 defmodule ChatterWeb.UserController do
   use ChatterWeb, :controller
   alias Chatter.User
-  alias Doorman.Auth.Secret
-  alias Chatter.Repo
   alias Doorman.Login.Session
+  alias Chatter.Accounts
 
   def new(conn, _params) do
-    changeset = User.changeset(%User{}, %{})
+    changeset = Accounts.change_user(%User{})
     render(conn, "new.html", changeset: changeset)
   end
 
   def create(conn, %{"user" => params}) do
-    changeset =
-      %User{}
-      |> User.changeset(params)
-      |> Secret.put_session_secret()
-
-    case Repo.insert(changeset) do
+    case Accounts.create_user(params) do
       {:ok, user} ->
         conn
         |> Session.login(user)
